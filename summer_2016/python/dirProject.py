46dd4470a5eb51063edc4102a0ba1966ab0965f7
Julia Cosma <jcosma@g.hmc.edu>
2016-09-22 20:24:41 -0700

    Added documentation to html generator

--- summer_2016/python/dirProject.py
+++ summer_2016/python/dirProject.py
@@ -5,56 +5,59 @@ import addRouteToMap, pointsFunction, intersectionParse, pygmaps
 import argparse
 
 
-# The Map
-THE_MAP = None
+# Initial map properties
 LAT = 34.09668
 LNG = -117.71978
 MAP_ZOOM = 15
 THE_MAP = pygmaps.maps(LAT, LNG, MAP_ZOOM)
 
 def main(kml_file, html_file, text_file):
+   
     # API key 
     gmaps = googlemaps.Client(key='AIzaSyDaPfA_TUTlbHLDH0K48qS-Jh2ETfCTz_0')
-    #for debugging
+    
+    #control for arrow colour: for debugging
     colors = ['#000000','#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF', '#FF00FF']
+    color_count = 0
+    
+    #get intersection data
     intersection_file = pointsFunction.kmlParse(kml_file,text_file)
-    intersections=intersectionParse.createIntersections(intersection_file)
-
-    mapOfNodes = {}
-    # directionIntersections = intersections
+    intersections = intersectionParse.createIntersections(intersection_file)
     directionIntersections = copy.deepcopy(intersections)
-    i = 0
-    # the school **look into changing it into the school logo*** add polygone here ***
 
+    #create an empty dict for the nodes
+    mapOfNodes = {}
+
+    #iterate through intersections
     while(directionIntersections):
-        temp_node = directionIntersections.pop()
-        start = [temp_node.lng, temp_node.lat]
         
+        #get our node from the intersection data
+        temp_node = directionIntersections.pop()
+        
+        #find the arrow endpoints
+        start = [temp_node.lng, temp_node.lat]
         end   = [34.10185, -117.71747]
 
         #making a call to the google maps api
         dirs = gmaps.directions(start, end, "walking")
-        #points = dirs[0]["overview_polyline"]["points"]
-
         points = [intersectionParse.Node(lng, lat) for lat, lng in polyline.decode(dirs[0]["overview_polyline"]["points"])]
-        # the distance of the route will possibly be helpful in the future if I am changing the colors of the arrows
+        # Note from Erin: the distance of the route will possibly be helpful in the future if I am changing the colors of the arrows
         points = addRouteToMap.snapToIntersection(points, intersections)
 
+        #add to map
         mapOfNodes = addRouteToMap.addRoute(mapOfNodes, intersections, directionIntersections, points)
 
-        i += 1
-        if i >20: 
-            # time.sleep(.5)
-            i =0
-            break
-    color_count = 0
+    # iterate through arrows
     for key in mapOfNodes:
         if(mapOfNodes[key] is not None):
-            print("Map leg: ", mapOfNodes[key], key)
-            print(key.lat, key.lng)
-            print(mapOfNodes[key].lat, mapOfNodes[key].lng)
+            
+            # Print statements for debugging:
+            #print("Map leg: ", mapOfNodes[key], key)
+            #print(key.lat, key.lng)
+            #print(mapOfNodes[key].lat, mapOfNodes[key].lng)
             THE_MAP.addpath([key, mapOfNodes[key]], key.value, colors[color_count])
-            # THE_MAP.addpath([mapOfNodes[key], key], key.value, colors[color_count])
+            
+            #Colour control
             color_count +=1
             if color_count == 6:
                 color_count = 0
