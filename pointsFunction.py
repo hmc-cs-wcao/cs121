9a253704cb3908d41232a2ddf9db43593bad42ae
mtkent <mtkent@hotmail.com>
2016-11-16 15:01:51 -0800

    Update to work for new Overpass query
    Will now work with getting all nodes - will combine parks, dead-ends, intersections

--- pointsFunction.py
+++ pointsFunction.py
@@ -1,32 +1,34 @@
-# Must take out the the link in the KML (of the file before this will work). **POSSIBLY FIX THIS***
+# FOR MYMAPS ONLY: Must take out the the link in the KML (of the file before this will work). **POSSIBLY FIX THIS***
+
 # This function will take in a KML file that has points at each of the intersections and create a file 
 # that has the lat and lng separated by commas and each of these separated by a new line
 
 
 import xml.etree.ElementTree as ET
 
-#text file will have long, lat separated by new lines
-# has a problem with the files? maybe with the outfile?
-
 def kmlParse(kml, text_file="/Users/m/Documents/GitHub/cs121old/summer_2016/out.txt"):
 
     tree = ET.parse(kml)
     root = tree.getroot()
     
-    # just need to loop through entire file, should return correct output
-    #starts at 2, will go until null. Only need to do a while loop, I think. 
-    # while num != NULL, add root[0][num][0][0].text + \n to file
-    #text_file.close
-
-    # also check to see if it's just taking out the link that is the problem
-    # document, description/placemark, point, coordinate
+    # starts at 2, since is first index with coordinate, will go until last element. 
+    # basically: while num != NULL, add root[0][num][0][0].text + \n to file
+    # indexes represent: document, description/placemark/name*, point, coordinate
+    # * this index can also represent the name of the school, which is why added the part where third index is 1
     num = 2
-    while root[0][num][0][0] != root[0][-1][0][0]:
-        text_file.write( (root[0][num][0][0]).text + '\n')
-        num += 1
 
+    while ( (root[0][num][1][0] != root[0][-1][1][0]) or (root[0][num][0][0] != root[0][-1][0][0])  ):
+        # if the tag is the name, set the third index to 1, not 0 
+        if (root[0][num][0].tag == '{http://www.opengis.net/kml/2.2}name'):
+             text_file.write( (root[0][num][1][0]).text + '\n')
+        else:
+            text_file.write( (root[0][num][0][0]).text + '\n')
+
+        # increment the coordinate being read
+        num += 1
+        
     
-    #will go through KML file - skips this step with the new file
+    #will go through KML file - skips this step with Overpass - only works for myMaps manual generation KML
     # for x in root.findall(".//Placemark"):
     #     # text_file.write(x.find("Point/coordinates").text + '\n')
     #     print ('im working!!!')
