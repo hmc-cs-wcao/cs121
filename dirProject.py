b7b4f51cf55ab6d439bf731a08b2234a9051193a
Julia Cosma <jcosma@g.hmc.edu>
2016-11-19 15:37:06 -0800

    Fixed some naming problems

---
+++ dirProject.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import googlemaps, polyline, copy
+import addRouteToMap, pointsFunction, intersectionParse, pygmaps
+import argparse
+import math
+
+
+# Initial map properties
+LAT = 34.1126 #34.09668  
+LNG = -117.7219 #-117.71978
+MAP_ZOOM = 15
+THE_MAP = pygmaps.maps(LAT, LNG, MAP_ZOOM)
+
+def main(kml_file, html_file, text_file):
+   
+    # API key 
+    gmaps = googlemaps.Client(key='AIzaSyDaPfA_TUTlbHLDH0K48qS-Jh2ETfCTz_0')
+    
+    #possible arrow colors
+    colors = ['#ff99ff','#ff00ff', '#660066']
+    color_count = 0
+    
+    #get intersection data
+    intersection_file = pointsFunction.kmlParse(kml_file,text_file)
+    intersections = intersectionParse.createIntersections(intersection_file)
+    directionIntersections = copy.deepcopy(intersections)
+
+    #create an empty dict for the nodes
+    mapOfNodes = {}
+
+    #iterate through intersections
+    while(directionIntersections):
+        
+        #get our node from the intersection data
+        temp_node = directionIntersections.pop()
+        
+        #find the arrow endpoints
+        start = [temp_node.lng, temp_node.lat]
+        end   = [LAT, LNG] # WHERE IS THIS FROM?
+
+        #making a call to the google maps api
+        dirs = gmaps.directions(start, end, "walking")
+        points = [intersectionParse.Node(lng, lat) for lat, lng in polyline.decode(dirs[0]["overview_polyline"]["points"])]
+        # Note from Erin: the distance of the route will possibly be helpful in the future if I am changing the colors of the arrows
+        points = addRouteToMap.snapToIntersection(points, intersections)
+
+        #add to map
+        mapOfNodes = addRouteToMap.addRoute(mapOfNodes, intersections, directionIntersections, points)
+
+    # iterate through arrows
+    for key in mapOfNodes:
+        if(mapOfNodes[key] is not None):
+
+            markerLAT = LNG
+            markerLNG = LAT
+
+            dist = math.sqrt( (abs((markerLAT - key.lat) * (markerLAT - key.lat)) )  + (abs((markerLNG - key.lng) * (markerLNG - key.lng)) )  )
+            
+            if (dist <= 0.00477071646):
+                color_count = 0
+            elif (dist <= 0.00954143292):
+                color_count = 1
+            else:
+                color_count = 2
+
+            THE_MAP.addpath([key, mapOfNodes[key]], key.value, colors[color_count])
+
+            
+    THE_MAP.addpoint(LAT, LNG, "#FF0000")
+
+    THE_MAP.draw(html_file)
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--kml','-k', metavar='FILE', type=argparse.FileType('r'),
+                        required=True,
+                   help='Read intersections from kml FILE')
+    parser.add_argument('--html','-m', metavar='FILE', type=argparse.FileType('w'),
+                        required=True,
+                        help='Write the output to html FILE')
+    parser.add_argument('--text','-t', metavar='FILE', type=argparse.FileType('w'),
+                        required=True,
+                        help='Write the text version of the coordinates to FILE')
+
+    args = parser.parse_args()
+    main(args.kml, args.html, args.text)
--- updateJS.py
+++ updateJS.py
@@ -9,12 +9,8 @@ def main():
 	#dirProject = imp.load_source('dirProject', './summer_2016/python/dirProject.py')
 	#addRouteToMap = imp.load_source('addRouteToMap', './summer_2016/python/addRouteToMap.py')	
 
-	#Execute pygmaps
-	#dirProject.main("./summer_2016/python/KML/IntersectionPoints.kml", "./summer_2016/python/dirProject.html", "./summer_2016/python/out.txt")
-	subprocess.call(["python", "./summer_2016/python/dirProject.py", "-k", "./summer_2016/python/KML/IntersectionPoints.kml", "-m", "./summer_2016/python/dirProject.html", "-t", "./summer_2016/python/out.txt"])
-
 	#Save script
-	with open("./summer_2016/python/dirProject.html", 'r') as htmlfile:
+	with open("./dirProject.html", 'r') as htmlfile:
 		html = htmlfile.read()
 	jsPattern = re.compile("(?<=<script type=\"text\/javascript\">)([^<])+")
 	findScript = jsPattern.search(html)
