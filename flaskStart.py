ed7231f681fff1f53a2f02b236a7299bec4e40cc
Julia Cosma <jcosma@g.hmc.edu>
2016-12-08 19:15:07 -0800

    MapSwitch now commented out

--- flaskStart.py
+++ flaskStart.py
@@ -1,4 +1,17 @@
 from flask import Flask, render_template
+import pyrebase
+
+firebaseConfig = {
+	"apiKey": "AIzaSyAogjWCe3ZwjkRjfaZ2cug44yCCDoJoHSM",
+	"authDomain": "active-transportation-maps.firebaseapp.com",
+	"databaseURL": "https://active-transportation-maps.firebaseio.com",
+	"storageBucket": "active-transportation-maps.appspot.com",
+	"messagingSenderId": "577755931894"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+storage = firebase.storage()
 
 app = Flask(__name__)
 
@@ -9,6 +22,15 @@ def about():
 @app.route('/map/')
 def map():
     return render_template('map.html')
+
+def mapSwitch(id):
+	"""takes in string uuid of school and replaces mapScript.js
+	    with that school's map from Firebase"""
+	print(id)
+	pathtoMap = id + "/mapScript.js"
+	storage.child(pathtoMap).put("static/js/mapScript.js")
+
+app.jinja_env.globals.update(mapSwitch=mapSwitch) # assign mapSwitch() as a Flask global
 
 if __name__ == '__main__':
 	# app.run(host='0.0.0.0')
